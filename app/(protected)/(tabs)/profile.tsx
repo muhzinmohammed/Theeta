9d7f0a97841d2a0bdb3b2cc78032a9e412f54c4b
muhzin mohammed <muhzin314in@gmail.com>
2025-06-21 22:47:11 +0530

    profile page update

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -1,13 +1,13 @@
-import Button from '@/components/Button'
-import { AuthContext } from '@/utils/authContext'
-import React, { useContext } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import Button from '@/components/Button';
+import { AuthContext } from '@/utils/authContext';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function profile() {
   const authContext = useContext(AuthContext);
   return (
     <View style={styles.conatiner}>
-      <Text>profile</Text>
+      <Text style={styles.subtitle}>profile</Text>
       <Button name="LogOut" style={styles.button} onPress={authContext.logOut}/>
     </View>
   )
@@ -15,17 +15,23 @@ export default function profile() {
 
 const styles = StyleSheet.create({
   conatiner: {
+    backgroundColor:'rgb(22, 13, 0)',
     justifyContent:'center',
     alignItems:'center',
     height:'100%'
   },
+  subtitle: {
+    color: 'rgb(219, 157, 98)',
+    fontSize: 32,
+  },
   button: {
-    backgroundColor:'blue',
+    backgroundColor: 'rgb(190, 109, 2)',
     color:'white',
-    height:40,
-    width:150,
-    borderRadius:20,
+    height:45,
+    width:250,
+    borderRadius:18,
     alignItems:'center',
-    justifyContent:'center'
-  }
+    justifyContent:'center',
+    marginTop:20
+  },
 })
